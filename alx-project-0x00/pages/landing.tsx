920133ef7ad4eb97cca858ce35c741846c5f7cb6
Adedayoke <adedayoke2006@gmail.com>
2025-09-19 04:52:18 +0100

    few submission error fixed

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,10 +6,10 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button title="Book Now" style="bg-blue-500 text-white rounded-sm px-4 py-2" />
-      <Button title="CLick Now" style="bg-blue-500 text-white rounded-md px-4 py-2" />
-      <Button title="Bye for Now" style="bg-blue-500 text-white rounded-full px-4 py-2" />
-      <Button title="Hello for Now" style="bg-blue-500 text-white rounded-md px-4 py-2" />
+      <Button title="Book Now" styles="bg-blue-500 text-white rounded-sm px-4 py-2" />
+      <Button title="CLick Now" styles="bg-blue-500 text-white rounded-md px-4 py-2" />
+      <Button title="Bye for Now" styles="bg-blue-500 text-white rounded-full px-4 py-2" />
+      <Button title="Hello for Now" styles="bg-blue-500 text-white rounded-lg px-4 py-2" />
     </div>
   )
 }
